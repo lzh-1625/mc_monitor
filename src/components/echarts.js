c69421675d24faa686035c642c68a52386a257d0
lzh <1625167628@qq.com>
2023-06-11 23:39:15 +0800

    modified:   package-lock.json 	modified:   package.json 	new file:   src/components/echarts.js 	new file:   src/img/wood.png 	modified:   src/page/home/index.js 	new file:   src/router/index.js 	modified:   src/router/router.js

---
+++ src/components/echarts.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import * as echarts from "echarts";
+import Card from "@material-ui/core/Card";
+import CardContent from '@material-ui/core/CardContent';
+import { CardHeader } from "@material-ui/core";
+
+export default class Chart extends Component {
+  constructor(){
+    super()
+
+  }
+  componentDidMount() {
+    this.getOption();
+  }
+  getOption = () => {
+    var myChart = echarts.init(document.getElementById("main"));
+    myChart.setOption({
+      xAxis: {
+        type: "category",
+        boundaryGap: false,
+        data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+      },
+      yAxis: {
+        minInterval: 1,
+        name: "在线人数",
+        type: "value",
+      },
+      series: [
+        {
+          data: [820, 932, 901, 934, 1290, 1330, 1320],
+          type: "line",
+          areaStyle: {},
+        },
+      ],
+    });
+  };
+
+  render() {
+    return (
+      <Card style={{"width":"60%","marginLeft":"20%","marginTop":"10px","marginBottom":"10px"}}>
+        <CardHeader title={"asdasdsa"}></CardHeader>
+        <CardContent >
+          <div id="main" style={{ height: "300px",width:"auto" }}></div>
+        </CardContent>
+      </Card>
+    );
+  }
+}
--- src/router/router.js
+++ src/router/router.js
@@ -1,31 +1,27 @@
-import { Routes, Route } from "react-router-dom";
+
 import Login from "../page/login/index";
-import React from "react";
-import { createTheme, ThemeProvider } from "@material-ui/core/styles";
-import useMediaQuery from "@material-ui/core/useMediaQuery";
 import Home from "../page/home/index";
-
-function App() {
-  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
-
-  const theme = React.useMemo(
-    () =>
-      createTheme({
-        palette: {
-          type: prefersDarkMode ? "dark" : "light",
-        },
-      }),
-    [prefersDarkMode]
-  );
-  return (
-    <ThemeProvider theme={theme}>
-      <switch>
-        <Routes>
-          <Route path="/login" exact element={<Login />} />
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </switch>
-    </ThemeProvider>
-  );
+import Router from "./index";
+import * as React from "react";
+import { useRoutes } from 'react-router-dom'
+export default function App() {
+    const element = useRoutes(
+        [
+            {
+                path: '/',
+                element: <Router></Router>,
+                children: [
+                    {
+                        path: 'Home',
+                        element: <Home />,
+                    },
+                ],
+            },
+            {
+                path: 'Login',
+                element: <Login />,
+            }
+        ]
+    )
+    return element
 }
-export default App;
